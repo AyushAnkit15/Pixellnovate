e42d55372488946b8d8dc9981093a59ccf75dc14
AyushAnkit15 <593ayush.2020@gmail.com>
2023-11-10 05:27:56 +0530

    some minor

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,11 +7,12 @@ dotenv.config();
 
 const router = express.Router();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+  });
 
 router.get('/' ,async(req ,res)=>{
     try{
